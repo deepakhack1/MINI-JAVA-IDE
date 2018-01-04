99a480805122c370ebece7beed6bfa972cdb803b
deepak-ghariyav <31047165+deepak-ghariyav@users.noreply.github.com>
2018-01-05 01:30:07 +0530

    Initial commit

---
+++ Cons.java
@@ -0,0 +1,43 @@
+class Cons
+{
+int x;
+Cons()
+{
+this('a');
+this.x=20;
+System.out.println(x);
+}
+Cons(int y)
+{
+System.out.println(y);
+}
+
+public static void main(String... args)
+{
+Cons t=new Cons();
+
+}
+
+}
+class Cons
+{
+int x;
+Cons()
+{
+this('a');
+this.x=20;
+System.out.println(x);
+}
+Cons(int y)
+{
+System.out.println(y);
+}
+
+public static void main(String... args)
+{
+Cons t=new Cons();
+
+}
+
+}
+
---
+++ H.java
@@ -0,0 +1,6 @@
+public class H {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
